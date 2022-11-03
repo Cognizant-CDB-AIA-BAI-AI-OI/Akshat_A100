88d2b570d0203b9685d058d85ec2448e83912f61
AkshatSharma_CTS <91590729+919294AkshatSharma@users.noreply.github.com>
2022-11-03 15:00:47 +0530

    Add files via upload

---
+++ funci.py
@@ -0,0 +1,18 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+tokenizer = AutoTokenizer.from_pretrained("fidukm34/biobert_v1.1_pubmed-finetuned-ner-finetuned-ner")
+
+model = AutoModelForTokenClassification.from_pretrained("fidukm34/biobert_v1.1_pubmed-finetuned-ner-finetuned-ner")
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+
+with open("Akshat_A100/example.txt", 'r') as f:
+	example = f.read()
+f.close()
+
+ner_results = nlp(example)
+
+with open("outputs/result.txt",'w+') as f:
+	f.write(ner_results)
+
+f.close()
+	
