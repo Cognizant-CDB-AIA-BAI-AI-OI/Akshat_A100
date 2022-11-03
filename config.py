b46b69318044be088103a245d19410fbd05ef1c0
AkshatSharma_CTS <91590729+919294AkshatSharma@users.noreply.github.com>
2022-11-03 16:02:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ def config():
   try :
     import sys 
     subprocess.run('conda env create -f Akshat_A100/env/env_akshat.yml python=3.8',shell =True)
-    subprocess.run('conda run -n env_akshat python Akshat_A100/funci.py',shell =True)
+    subprocess.run('conda run -n env_akshat python Akshat_A100/func.py',shell =True)
     subprocess.run('conda env remove -n env_akshat',shell =True)
   except :
     print("..Something went wrong..!!")
