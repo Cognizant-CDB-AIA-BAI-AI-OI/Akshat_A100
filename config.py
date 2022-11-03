882246d938a722ff00ca35596bb5153b474621d8
Mukesh18S <91795523+Mukesh18S@users.noreply.github.com>
2022-11-03 16:40:54 +0530

    akshat_test9 --run

--- config.py
+++ config.py
@@ -3,14 +3,47 @@ def config():
   try :
     import sys 
     subprocess.run('conda env create -f Akshat_A100/env/env_akshat.yml python=3.8',shell =True)
-    subprocess.run('conda run -n env_akshat python Akshat_A100/funci.py',shell =True)
-    subprocess.run('conda env remove -n env_akshat',shell =True)
+  
   except :
-    print("..Something went wrong..!!")
+    
+    
+    f = open('outputs/a_test_dile.txt','w+')
+    f.write(str(sys.exc_info()[0]))
+    f.close()
+  
+  else :
+  
+    f = open('outputs/a_test_dile.txt','w+')
+    f.write('Environment Created')
+    f.close()
+  
+    #time.sleep(150)
+    
+    try :
+      
+      subprocess.run('conda run -n env_akshat python Akshat_A100/funci.py',shell =True)
+    
+    except :
+      f = open('outputs/a_test_dile.txt','a+')
+      f.write(str(sys.exc_info()[0]))
+      f.close()
+    
+    try :
+      
+      subprocess.run('conda env remove -n env_akshat',shell =True)
+    
+    except :
+      f = open('outputs/a_test_dile.txt','a+')
+      f.write(str(sys.exc_info()[0]))
+      f.close()
+      
+    else :
+      f = open('outputs/a_test_dile.txt','a+')
+      f.write('Environment Deleted')
+      f.close()
+  
 
-    
-    
-    
+     
     
     
     
