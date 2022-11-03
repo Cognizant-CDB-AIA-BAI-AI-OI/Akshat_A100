c01687df83124b9c7b682494404c7fbce23180fd
AkshatSharma_CTS <91590729+919294AkshatSharma@users.noreply.github.com>
2022-11-03 15:11:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import subprocess
+def config():
+  try :
+    import sys 
+    subprocess.run('conda env create -f Akshat_A100/env/env_akshat.yml python=3.8',shell =True)
+    subprocess.run('conda run -n env_akshat python Akshat_A100/funci.py',shell =True)
+    subprocess.run('conda env remove -n env_akshat',shell =True)
+  except :
+    print("Something went wrong")
